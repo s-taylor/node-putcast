d4f9385cbfa5c76dd41644f4f61c202513734315
Simon Taylor <nizmox@gmail.com>
2015-06-16 19:32:09 +1000

    refactor to make oauth token a param

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ var app = express()
 
 var flatten = require('./lib/flatten.js')
 
-app.get('/', function (req, res) {
-  flatten({ minSize: 100000000 })
+app.get('/html/:token', function (req, res) {
+  var token = req.params.token;
+  if (!token) throw new Error("oauth token not provided");
+  flatten(token, { minSize: 100000000 })
     .then(function(result) {
       //console.log('result', result)
-      var view = __dirname + '/views/index.jade';
-      var locals = {
-        files: result
-      }
+      var view = __dirname + '/views/html.jade';
+      var locals = {}
+      locals.files = result
+      res.render(view, locals);
+    })
+})
+
+app.get('/rss', function (req, res) {
+  flatten({ minSize: 100000000 })
+    .then(function(result) {
+      var view = __dirname + '/views/html.jade';
+      var locals = {}
+      locals.files = result
       res.render(view, locals);
     })
 })
