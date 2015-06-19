a54d734a1222838c5ca929df333083f5e04482ba
Simon Taylor <nizmox@gmail.com>
2015-06-19 17:56:36 +1000

    working version of html and rss

--- server.js
+++ server.js
@@ -16,14 +16,22 @@ app.get('/html/:token', function (req, res) {
     })
 })
 
-app.get('/rss', function (req, res) {
-  flatten({ minSize: 100000000 })
+app.get('/rss/:token', function (req, res) {
+  var token = req.params.token;
+  if (!token) throw new Error("oauth token not provided");
+  flatten(token, { minSize: 100000000 })
     .then(function(result) {
-      var view = __dirname + '/views/html.jade';
+      //console.log('result', result)
+      var view = __dirname + '/views/rss.jade';
       var locals = {}
+      locals.feedUrl = getFullUrl(req);
       locals.files = result
       res.render(view, locals);
     })
 })
 
 app.listen(3000)
+
+var getFullUrl = function(req) { 
+  return req.protocol + '://' + req.get('host') + req.originalUrl;
+}
