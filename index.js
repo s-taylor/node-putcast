1ad108d776c39ba7ac858d84864181425e6fffa0
Simon Taylor <nizmox@gmail.com>
2016-06-11 16:04:13 +1000

    lint index.js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var flatten = require('./lib/flatten.js');
+const flatten = require('./lib/flatten.js');
 
-app.get('/html/:token', function (req, res) {
-  var token = req.params.token;
-  if (!token) throw new Error("oauth token not provided");
-  var opts = req.query;
+function getFullUrl(req) {
+  return `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+}
+
+app.get('/html/:token', (req, res) => {
+  const token = req.params.token;
+  if (!token) throw new Error('oauth token not provided');
+  const opts = req.query;
   //TODO - write function to sanitize query opts
   if (opts.minSize) opts.minSize = parseInt(opts.minSize);
   flatten(token, opts)
-    .then(function(result) {
-      //console.log('result', result)
-      var view = __dirname + '/views/html.jade';
-      var locals = {};
-      locals.files = result;
-      res.render(view, locals);
-    });
+  .then((result) => {
+    //console.log('result', result)
+    const view = `${__dirname}/views/html.jade`;
+    const locals = {};
+    locals.files = result;
+    res.render(view, locals);
+  });
 });
 
-app.get('/rss/:token', function (req, res) {
-  var token = req.params.token;
-  if (!token) throw new Error("oauth token not provided");
-  var opts = req.query;
+app.get('/rss/:token', (req, res) => {
+  const token = req.params.token;
+  if (!token) throw new Error('oauth token not provided');
+  const opts = req.query;
   //TODO - write function to sanitize query opts
   if (opts.minSize) opts.minSize = parseInt(opts.minSize);
   flatten(token, opts)
-    .then(function(result) {
-      //console.log('result', result)
-      var view = __dirname + '/views/rss.jade';
-      var locals = {};
-      locals.feedUrl = getFullUrl(req);
-      locals.files = result;
-      res.render(view, locals);
-    });
+  .then((result) => {
+    //console.log('result', result)
+    const view = `${__dirname}/views/rss.jade`;
+    const locals = {};
+    locals.feedUrl = getFullUrl(req);
+    locals.files = result;
+    res.render(view, locals);
+  });
 });
 
 //start server
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
-console.log("Server started on port " + port);
-
-var getFullUrl = function(req) { 
-  return req.protocol + '://' + req.get('host') + req.originalUrl;
-};
+console.log(`Server started on port ${port}`);
